4bfe1c6a5ffdf4b05a73255b6cd55c8e5b0f27f0
Haisen Zhao <haisenzhao@gmail.com>
2021-09-30 22:05:45 +0200

    update

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std；
+
+int main(int argc, char* argv[])
+{
+	system("pause");
+	return 0;
+}
+
