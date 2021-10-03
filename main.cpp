45f5d49ab71399fff90df92c1006ebeb8a138002
Haisen Zhao <haisenzhao@gmail.com>
2021-10-03 15:57:56 +0200

    test-username

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ using namespace PGL;
 
 int main(int argc, char* argv[])
 {
+	std::cerr << Functs::WinGetCurDirectory() << std::endl;
+	std::cerr << Functs::WinGetUserName() << std::endl;
 	Functs::MAssert("Test pgl library...");
 	system("pause");
 	return 0;
