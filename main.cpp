f08ae06fbba02746b6535ab188b307957aaf5fe8
Haisen Zhao <haisenzhao@gmail.com>
2021-12-26 12:53:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,16 @@
 using namespace std;
 using namespace PGL;
 
+//#include <sys/stat.h>
+//#include <string>
+//#include <fstream>
+
+inline bool exists_test3(const std::string& name) {
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0);
+}
+
+
 int main(int argc, char* argv[])
 {
 	std::cerr <<"WinGetCurDirectory: " << Functs::WinGetCurDirectory() << std::endl;
@@ -18,6 +28,11 @@ int main(int argc, char* argv[])
 	auto b = Functs::DetectExisting("E:\\Dropbox\\Mold\\microstructures\\demo\\pipeline\\3_accessible2");
 	Functs::CerrLine(std::to_string(a));
 	Functs::CerrLine(std::to_string(b));
+
+	auto c = exists_test3("C:\\Working");
+	auto d = exists_test3("C:\\expert.txt");
+	Functs::CerrLine(std::to_string(c));
+	Functs::CerrLine(std::to_string(d));
 
 	system("pause");
 	return 0;
