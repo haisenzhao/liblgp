2da92be21d4a0e50147a67558cc33a0dc76733d8
Haisen Zhao <haisenzhao@gmail.com>
2022-01-01 17:33:07 +0100

    update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "pgl_functs.hpp"
 #include "RI.hpp"
 #include "tinyxml2.hpp"
-
+//#include "tinyxml2.h"
 using namespace std;
 using namespace PGL;
 
@@ -13,26 +13,17 @@ using namespace PGL;
 //#include <string>
 //#include <fstream>
 
-inline bool exists_test3(const std::string& name) {
-	struct stat buffer;
-	return (stat(name.c_str(), &buffer) == 0);
-}
-
-
 int main(int argc, char* argv[])
 {
 	std::cerr <<"WinGetCurDirectory: " << Functs::WinGetCurDirectory() << std::endl;
 	std::cerr << "WinGetUserName: " << Functs::WinGetUserName() << std::endl;
 	
+   // CC().LoadConfig(argc, argv);
+    
 	auto a = Functs::DetectExisting("E:\\Dropbox\\Mold\\microstructures\\demo\\pipeline\\3_accessible");
 	auto b = Functs::DetectExisting("E:\\Dropbox\\Mold\\microstructures\\demo\\pipeline\\3_accessible2");
 	Functs::CerrLine(std::to_string(a));
 	Functs::CerrLine(std::to_string(b));
-
-	auto c = exists_test3("C:\\Working");
-	auto d = exists_test3("C:\\expert.txt");
-	Functs::CerrLine(std::to_string(c));
-	Functs::CerrLine(std::to_string(d));
 
 	system("pause");
 	return 0;
