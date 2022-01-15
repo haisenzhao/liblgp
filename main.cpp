d9c63e2252e670b3df9581540204dea21604a982
Haisen Zhao <haisenzhao@gmail.com>
2022-01-15 21:01:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,41 +13,10 @@
 using namespace std;
 using namespace PGL;
 
-void CClearFolder(const std::string& path)
-{
-	if (!FF::DetectExisting(path))
-	{
-		auto folders = FF::SplitStr(FF::StringReplace(path, "\\", "/"), "/");
-		if (folders.back() == "")
-			folders.erase(folders.begin()+folders.size()-1);
-		std::string str;
-		for (int i = 0; i < folders.size(); i++)
-		{
-			str += folders[i] + "/";
-			if (!FF::DetectExisting(str))
-			{
-				if (_mkdir(str.c_str())) {};
-			}
-		}
-	}
-	else
-	{
-		std::string del_cmd = "del /f/s/q " + path + " > nul";
-		system(del_cmd.c_str());
-		std::string rmdir_cmd = "rmdir /s/q " + path;
-		system(rmdir_cmd.c_str());
-
-		if (_mkdir(path.c_str())) {};
-	}
-}
-
-
 int main(int argc, char* argv[])
 {
 	std::cerr <<"WinGetCurDirectory: " << Functs::WinGetCurDirectory() << std::endl;
 	std::cerr << "WinGetUserName: " << Functs::WinGetUserName() << std::endl;
-	
-	CClearFolder("C:\\123\\456");
 
    // CC().LoadConfig(argc, argv);
     
